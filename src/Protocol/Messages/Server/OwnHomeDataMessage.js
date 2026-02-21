e5a68f3ad9f86141c04e6c7088dae75f08b1b632
Banaanae <83927639+Banaanae@users.noreply.github.com>
2026-02-21 17:11:50 +1100

    ohd structure

--- src/Protocol/Messages/Server/OwnHomeDataMessage.js
+++ src/Protocol/Messages/Server/OwnHomeDataMessage.js
@@ -9,10 +9,58 @@ class OwnHomeDataMessage extends PiranhaMessage {
     }
 
     async encode() {
-        this.stream.writeInt(1)
-        this.stream.writeLong(0, 1)
-        // (a1 + 160) + 24LL)
-        this.stream.writeInt(1)
+        this.stream.writeInt(Date.now())
+        this.stream.writeLongLong(0, 1)
+
+        // LogicClientAvatar::encode
+        // 7AB9E8
+
+        this.stream.writeLongLong(0, 1)
+        this.stream.writeStringReference("Banaanae")
+        this.stream.writeInt(3) // reg state
+        this.stream.writeVInt(0)
+        this.stream.writeVInt(0)
+        this.stream.writeVInt(0)
+        this.stream.writeVInt(0)
+
+        // 683D00
+
+        //this.stream.writeHex(decoder.decode(new Uint8Array([0x02, 0xE7])))
+
+        this.stream.writeBoolean(true)
+        {
+            this.stream.writeLongLong(0, 1)
+        }
+
+        this.stream.writeBoolean(true)
+        this.stream.writeString("")
+        this.stream.writeInt(0)
+        this.stream.writeInt(0)
+        this.stream.writeInt(0)
+        this.stream.writeInt(0)
+        this.stream.writeInt(0)
+
+        // v5 = (a1 + 80)
+        // v5 > 0
+        this.stream.writeBoolean(false)
+        if (false) {
+            this.stream.writeString("")
+        } else {
+            this.stream.writeBytes(Buffer.from([0x00])) // a1 + 80
+        }
+
+        this.stream.writeLongLong(0, 1)
+        this.stream.writeLongLong(0, 1)
+        this.stream.writeBoolean(false)
+        this.stream.writeInt(0)
+        this.stream.writeBoolean(false)
+        this.stream.writeInt(0)
+        this.stream.writeLongLong(0, 1)
+        this.stream.writeVInt(0)
+        this.stream.writeLongLong(0, 1)
+        this.stream.writeVInt(0)
+
+        this.stream.writeInt(0)
     }
 }
 
