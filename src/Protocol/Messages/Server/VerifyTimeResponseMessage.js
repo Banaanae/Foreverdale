2f1451641c9c845419e6906165cdb150fd4aaa45
Banaanae <83927639+Banaanae@users.noreply.github.com>
2026-02-26 16:47:30 +1100

    improved handling

--- src/Protocol/Messages/Server/VerifyTimeResponseMessage.js
+++ src/Protocol/Messages/Server/VerifyTimeResponseMessage.js
@@ -9,13 +9,9 @@ class VerifyTimeResponseMessage extends PiranhaMessage {
   }
 
   async encode() {
-    // 1️⃣ First long = echoed from client
-    this.stream.writeLongLong(0n)
+    for (let i = 0; i < 5; i++)
+        this.stream.writeLongLong(0n)
 
-    // 2️⃣ Next 4 longs = placeholders (zeros)
-    for (let i = 0; i < 4; i++) this.stream.writeLongLong(0n)
-
-    // 3️⃣ Sixth long = monotonic server timestamp
     const serverTime = process.hrtime.bigint()
     this.stream.writeLongLong(serverTime)
   }
